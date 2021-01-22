28334eb101d3358b70fefd1776cd9f54bb4e9465
Dhriti18 <73419650+Dhriti18@users.noreply.github.com>
2021-01-22 17:08:01 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -23,15 +23,17 @@ bob5=new Bob(600,500,70);
 bob4=new Bob(530,500,70);
 bob3=new Bob(460,500,70);
 bob2=new Bob(390,500,70);
-bob1=new Bob(320,500,71);
+//bob1=new Bob(320,500,71);
 roof1=new Roof(440,200,360,40);
-String1=new Rope (roof1.body,bob1.body);
-String2=new Rope (roof1.body,bob2.body);
-String3=new Rope (roof1.body,bob3.body);
-String4=new Rope (roof1.body,bob4.body);
-String5=new Rope (roof1.body,bob5.body);
 
-//rope1=new Rope (bob1.body,bob5.body);
+String1=new Rope (roof1.body,bob1.body,-100);
+String2=new Rope (roof1.body,bob2.body,-50);
+String3=new Rope (roof1.body,bob3.body,0);
+String4=new Rope (roof1.body,bob4.body,50);
+String5=new Rope (roof1.body,bob5.body,100);
+
+
+
 //console.log(rope1);
 
 //b1=new Roof(440,200,360,40);
@@ -57,10 +59,21 @@ function draw() {
   String3.display();
   String4.display();
   String5.display();
+
   //.display();
   drawSprites();
  
 }
 
+function keyPressed(){
+if(keyCode===UP_ARROW){
 
+// Matter.    //Matter.Body.setStatic(bob1.body, {isStatic:false})
+        console.log("hi");
+        Body.applyForce(bob1.body, bob1.body.position, {x:-90});
+         Matter.Body.setStatic(bob1.body, {isStatic:false})
 
+	}
+	
+	
+}
